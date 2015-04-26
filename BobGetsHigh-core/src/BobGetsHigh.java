f3fe1f0568620a9cd1aad6508456a69cd978035b
Ivo Ilić <admin@ivoilic.com>
2015-04-26 16:47:05 -0500

    Some changes

--- BobGetsHigh-core/src/BobGetsHigh.java
+++ BobGetsHigh-core/src/BobGetsHigh.java
@@ -1,23 +1,26 @@
 import java.io.FileReader;
 import java.util.Iterator;
- 
+import java.util.List;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 
 
-
+@SuppressWarnings("unchecked")
 public class BobGetsHigh
 {
 
 	public static void main(String[] args)
 	{
-			
+		generateEvents();
 	}
 	
-	public void generateEvents()
+	//Thank you: http://crunchify.com/how-to-read-json-object-from-file-in-java/
+	public static void generateEvents()
 	{
-		
+	
 	}
 
 }
