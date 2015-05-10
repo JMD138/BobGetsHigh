9995e9f0ea14045603cfb9afd4a4d9869d763439
Derrick Foti <derrickfoti@gmail.com>
2015-05-10 16:11:03 -0500

    project

--- BobGetsHigh-core/src/BobGetsHigh.java
+++ BobGetsHigh-core/src/BobGetsHigh.java
@@ -66,7 +66,7 @@ public class BobGetsHigh
 	{
 		generateGame();
 		makeFrame();
-		setDisplay("Map");
+		setDisplay("Event");
 	}
 	
 	public void makeFrame()
@@ -217,19 +217,7 @@ public class BobGetsHigh
 		invisibleButton.setVisible(false);
 		frame.add(invisibleButton);
 		frame.repaint();
-	}
-	
-	public void makeMap(){
-		for(int i = 0; i < locations.size(); i++){
-			JButton locationButton = new JButton(currentEvent.getOptions(0));
-			locationButton.setVerticalTextPosition(AbstractButton.CENTER);
-			locationButton.setHorizontalTextPosition(AbstractButton.LEADING);
-			locationButton.setBounds(button3X,100+(i*50), BUTTON_WIDTH, BUTTON_HEIGHT);
-			locationButton.setText(locations.get(i).getLocationName());
-			frame.add(locationButton);
-			frame.repaint();
-		}
-	}
+	}   
 
 	
 	
@@ -422,10 +410,7 @@ public class BobGetsHigh
 		}
 		else if(mode == "Map"){
 			updateStoryText("");
-			frame.remove(storyText);
-			makeMap();
-			frame.add(storyText);
-			frame.repaint();
+			
 		}
 		
 	}
